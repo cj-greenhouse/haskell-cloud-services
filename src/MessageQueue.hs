56ecfadfc18b36c1016028306c233592dfc7109b
Jeff Windsor <jeff.windsor@gmail.com>
2019-04-20 17:13:46 -0700

    Message Queue recieve message

--- src/MessageQueue.hs
+++ src/MessageQueue.hs
@@ -6,23 +6,25 @@ module MessageQueue (
     deleteMessageInSQS
 ) where
 --
-import Control.Monad (void)
-import Control.Monad.Trans.AWS (runAWST)
-import Data.Text (Text)
-import Network.AWS (runResourceT, send)
-import Network.AWS.Env (Env)
-import qualified Network.AWS.SQS.DeleteMessage as SQS (deleteMessage)
-import qualified Network.AWS.SQS.ReceiveMessage as SQS (receiveMessage)
-import qualified Network.AWS.SQS.SendMessage as SQS (sendMessage)
+import Control.Lens                                    ((^.))
+import Control.Monad                                   (void)
+import Control.Monad.Trans.AWS                         (runAWST)
+import Data.Text                                       (Text)
+import Network.AWS                                     (runResourceT, send)
+import Network.AWS.Env                                 (Env)
+import qualified Network.AWS.SQS.DeleteMessage  as SQS (deleteMessage)
+import qualified Network.AWS.SQS.ReceiveMessage as SQS (receiveMessage, rmrsMessages)
+import qualified Network.AWS.SQS.SendMessage    as SQS (sendMessage)
+import Network.AWS.SQS.Types                           (mReceiptHandle, mBody)
 --
-type MessageBody            = Text      --SQS limits to 256KB
-type MessageHandle          = Text
-type MessageQueueContainer  = Text
+type MessageBody           = Text      --SQS limits to 256KB
+type MessageReceiptHandle  = Text
+type MessageQueueContainer = Text
 
 class MessageQueue m where
-    sendMessage     :: MessageQueueContainer -> MessageBody -> m ()
-    receiveMessage  :: MessageQueueContainer -> m (MessageHandle, MessageBody)
-    deleteMessage   :: MessageQueueContainer -> MessageHandle -> m ()
+    sendMessage    :: MessageQueueContainer -> MessageBody -> m ()
+    receiveMessage :: MessageQueueContainer -> m [(Maybe MessageReceiptHandle, Maybe MessageBody)]
+    deleteMessage  :: MessageQueueContainer -> MessageReceiptHandle -> m ()
 
 class SQSEnvironment m where
     sqsEnvironment :: m Env
@@ -33,14 +35,18 @@ sendMessageInSQS queueUrl messageBody = do
     env <- sqsEnvironment
     void $ runResourceT $ runAWST env $ send $ SQS.sendMessage queueUrl messageBody
 
-receiveMessageInSQS :: (Monad IO, SQSEnvironment IO) => MessageQueueContainer -> IO (MessageHandle, MessageBody)
+receiveMessageInSQS :: (Monad IO, SQSEnvironment IO) => MessageQueueContainer -> IO [Maybe (MessageReceiptHandle, MessageBody)]
 receiveMessageInSQS queueUrl = do
     env <- sqsEnvironment
     response <- runResourceT $ runAWST env $ send $ SQS.receiveMessage queueUrl
-    undefined
+    pure $ map toResult $ response ^. SQS.rmrsMessages
+    where
+        toResult r = case (r ^. mReceiptHandle, r ^. mBody) of
+            (Just h, Just b) -> Just (h, b)
+            _                -> Nothing
 
 --TODO return the response for inspection if needed??
-deleteMessageInSQS :: (Monad IO, SQSEnvironment IO) => MessageQueueContainer -> MessageHandle -> IO ()
+deleteMessageInSQS :: (Monad IO, SQSEnvironment IO) => MessageQueueContainer -> MessageReceiptHandle -> IO ()
 deleteMessageInSQS queueUrl messageHandle = do
     env <- sqsEnvironment
     void $ runResourceT $ runAWST env $ send $ SQS.deleteMessage queueUrl messageHandle
