e1cd2eac16cf7210ba53e91ca31eccd465f1897d
Jeff Windsor <jeff.windsor@gmail.com>
2019-04-20 17:40:32 -0700

    Object Store refactor

--- src/ObjectStore.hs
+++ src/ObjectStore.hs
@@ -7,33 +7,33 @@ module ObjectStore (
 ) where
 --
 import Conduit
-import Control.Exception(try, SomeException)
-import Control.Lens (view)
-import Control.Monad (void, join)
-import Control.Monad.Trans.AWS (sinkBody, runAWST, Rs)
-import qualified Control.Monad.Trans.AWS as AWS (send, paginate)
-import qualified Data.ByteString as B (ByteString, concat)
-import Data.Text (Text)
-import Network.AWS (runAWS, runResourceT)
-import Network.AWS.Data.Body (RqBody(Hashed), toHashed)
-import Network.AWS.Env (Env)
-import Network.AWS.Pager (AWSPager)
-import qualified Network.AWS.S3.GetObject as AWS (getObject, gorsBody)
-import qualified Network.AWS.S3.PutObject as AWS (putObject)
-import Network.AWS.S3.ListObjectsV2 (listObjectsV2, lovrsContents)
-import qualified Network.AWS.S3.Types as AWS (ObjectKey(..), BucketName(..), oKey, keyName)
+import Control.Exception                            (try, SomeException)
+import Control.Lens                                 (view)
+import Control.Monad                                (void, join)
+import Control.Monad.Trans.AWS                      (sinkBody, runAWST, Rs)
+import qualified Control.Monad.Trans.AWS    as AWS  (send, paginate)
+import qualified Data.ByteString            as BS   (ByteString, concat)
+import Data.Text                                    (Text)
+import Network.AWS                                  (runAWS, runResourceT)
+import Network.AWS.Data.Body                        (RqBody(Hashed), toHashed)
+import Network.AWS.Env                              (Env)
+import Network.AWS.Pager                            (AWSPager)
+import qualified Network.AWS.S3.GetObject   as AWS  (getObject, gorsBody)
+import qualified Network.AWS.S3.PutObject   as AWS  (putObject)
+import Network.AWS.S3.ListObjectsV2                 (listObjectsV2, lovrsContents)
+import qualified Network.AWS.S3.Types       as AWS  (ObjectKey(..), BucketName(..), oKey, keyName)
 --
 type ObjectContainer = Text
 type ObjectKey       = Text
-type ObjectValue     = B.ByteString
+type ObjectValue     = BS.ByteString
 
 class ObjectStore m where
-    getObject   :: ObjectContainer -> ObjectKey -> m ObjectValue
-    putObject   :: ObjectContainer -> ObjectKey -> ObjectValue -> m ()
-    listObjects :: ObjectContainer -> m [ObjectKey]
+    getObject       :: ObjectContainer -> ObjectKey -> m ObjectValue
+    putObject       :: ObjectContainer -> ObjectKey -> ObjectValue -> m ()
+    listObjects     :: ObjectContainer -> m [ObjectKey]
 
 class S3Environment m where
-    s3Environment :: m Env
+    s3Environment   :: m Env
 
 -- | get content to bucket using provided S3 credentials
 getObjectInS3 :: (Monad IO, S3Environment IO) => ObjectContainer -> ObjectKey -> IO ObjectValue
@@ -42,10 +42,9 @@ getObjectInS3 bucketName objectKey = do
     l <- runResourceT . runAWST env $ do
         rs <- view AWS.gorsBody <$> (AWS.send $ AWS.getObject (AWS.BucketName bucketName) (AWS.ObjectKey objectKey))
         sinkBody rs sinkList
-    pure $ B.concat l
+    pure $ BS.concat l
 
 -- | put content to bucket using provided credentials
---TODO return the put response for inspection if needed??
 putObjectInS3 :: (Monad IO, S3Environment IO) => ObjectContainer -> ObjectKey -> ObjectValue -> IO ()
 putObjectInS3 bucketName objectKey content = do
     let req = AWS.putObject
