d2fd02f8373cd2a00c700360e2d095ad1818e7c9
Jeff Windsor <jeff.windsor@gmail.com>
2019-04-20 17:35:00 -0700

    Mail Exchange send mail response

--- src/MailExchange.hs
+++ src/MailExchange.hs
@@ -5,40 +5,36 @@ module MailExchange (
     sendMailInSES
 ) where
 --
-import Control.Monad (void)
-import Control.Monad.Trans.AWS (runAWST)
-import Control.Lens ((.~), set)
-import Data.Function ((&))
-import Data.Text (Text)
-import Network.AWS (runResourceT, send)
-import Network.AWS.Env (Env, newEnv, envRegion)
-import Network.AWS.SES (SendEmail, sendEmail)
-import Network.AWS.SES.Types (body, bText, content, destination, dToAddresses, message)
+import Control.Monad.Trans.AWS  (runAWST)
+import Control.Lens             ((^.), (.~))
+import Data.Function            ((&))
+import Data.Text                (Text)
+import Network.AWS              (runResourceT, send)
+import Network.AWS.Env          (Env)
+import Network.AWS.SES          (SendEmail, sendEmail, sersResponseStatus)
+import Network.AWS.SES.Types    (body, bText, content, destination, dToAddresses, message)
 --
-type MailToAddresses = [MailAddress]
-type MailFromAddress = MailAddress
-type MailSubject     = Text
-type MailBody        = Text
-newtype MailAddress  = EmailAddress { emailAddress :: Text } deriving (Eq, Show)
+type MailToAddresses        = [MailAddress]
+type MailFromAddress        = MailAddress
+type MailSubject            = Text
+type MailBody               = Text
+type MailAddress            = Text
+type SendMailResponseStatus = Int
 
 class MailExchange m where
-    sendMail :: MailFromAddress -> MailToAddresses -> MailSubject -> MailBody -> m ()
+    sendMail :: MailFromAddress -> MailToAddresses -> MailSubject -> MailBody -> m SendMailResponseStatus
 
 class SESEnvironment m where
     sesEnvironment :: m Env
 
---TODO return send mail response for inspection???
-sendMailInSES :: (Monad IO, SESEnvironment IO) => MailFromAddress -> MailToAddresses -> MailSubject -> MailBody -> IO ()
+sendMailInSES :: (Monad IO, SESEnvironment IO) => MailFromAddress -> MailToAddresses -> MailSubject -> MailBody -> IO SendMailResponseStatus
 sendMailInSES fromAddress toAddresses mailSubject mailBody = do
     env <- sesEnvironment
-    void $ runResourceT $ runAWST env $ send $ buildSESSendEmail
-        (emailAddress fromAddress)
-        (map emailAddress toAddresses)
-        (mailSubject)
-        (mailBody)
+    response <- runResourceT $ runAWST env $ send $ sesSendEmail fromAddress toAddresses mailSubject mailBody
+    pure $ response ^. sersResponseStatus
 
-buildSESSendEmail :: Text -> [Text] -> Text -> Text -> SendEmail
-buildSESSendEmail fromAddress toAddresses subjectText bodyText =
+sesSendEmail :: MailFromAddress -> MailToAddresses -> MailSubject -> MailBody -> SendEmail
+sesSendEmail fromAddress toAddresses subjectText bodyText =
     let
         destinationContent = destination & dToAddresses .~ toAddresses
         bodyContent = body & bText .~ Just (content bodyText)
