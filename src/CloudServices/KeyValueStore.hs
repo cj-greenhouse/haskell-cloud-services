ba18d58670efd95a82b05043cc94f86f04d5ebd8
Windsor, Jeffrey <jwindsor@cj.com>
2019-04-30 16:38:09 -0700

    KeyValueStore type class refactor

--- src/CloudServices/KeyValueStore.hs
+++ src/CloudServices/KeyValueStore.hs
@@ -1,14 +1,14 @@
 module CloudServices.KeyValueStore where
 --
-import Data.Text               (Text)
+import Data.Text (Text)
 
 type KeyValueContainer              = Text
 type KeyValuePutResponseStatus      = Int
 type KeyValueChangeResponseStatus   = Int
 type KeyValueDeleteResponseStatus   = Int
 
-class KeyValueStore m k v where
+class KeyValueStore m where
     getValue     :: KeyValueContainer -> k -> m v
     putValue     :: KeyValueContainer -> k -> v -> m KeyValuePutResponseStatus
     changeValue  :: KeyValueContainer -> k -> v -> m KeyValueChangeResponseStatus
-    -- deleteValue  :: forall v. KeyValueContainer -> k -> m KeyValueDeleteResponseStatus
+    deleteValue  :: KeyValueContainer -> k -> m KeyValueDeleteResponseStatus
